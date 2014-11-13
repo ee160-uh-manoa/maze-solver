f83cdda908211f04ffc6f64de6f1eea60596ece5
Zhao Li <zhaol@hawaii.edu>
2014-11-13 17:43:41 +0000

    reset possible_steps for each iteration

--- source/functions.h
+++ source/functions.h
@@ -2,6 +2,7 @@ void read_in(int maze[MAZE_HEIGHT][MAZE_WIDTH]);
 
 int *available_steps_at(struct Coordinate position, int maze[MAZE_HEIGHT][MAZE_WIDTH]);
 int *decode(int possible_steps_code);
+void reset(int possible_steps[MAX_POSSIBLE_STEPS]);
 int it_is_possible_to_go(int direction, int possible_steps_code);
 void add_to_possible_steps(int direction, int possible_steps[MAX_POSSIBLE_STEPS]);
 int strip_off_north(int possible_steps_code);
